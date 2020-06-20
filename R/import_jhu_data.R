b781f81df192cf8a08139353e6fec67fae58b2c5
Aaron Chafetz <achafetz@usaid.gov>
2020-06-20 15:59:48 -0400

    change type to type_url for use in renaming

--- R/import_jhu_data.R
+++ R/import_jhu_data.R
@@ -12,11 +12,11 @@ import_jhu_data <- function(type){
   stopifnot(length(type) != 0 & type %in% c("cases", "recoveries", "deaths"))
 
   # Select the type of data to pull
-  type <- dplyr::case_when(type == "cases" ~ tsCases,
+  type_url <- dplyr::case_when(type == "cases" ~ tsCases,
                            type == "recoveries" ~ tsRecov,
                            type == "deaths" ~ tsDeaths)
 
-  df <- readr::read_csv(file.path(paste0(jhuRepo, type)), col_types = c(.default = "d",
+  df <- readr::read_csv(file.path(paste0(jhuRepo, type_url)), col_types = c(.default = "d",
                                                                         `Province/State` = "c",
                                                                         `Country/Region` = "c"))
   df <- reshape_jhu_covid(df)
