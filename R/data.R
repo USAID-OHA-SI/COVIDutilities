3e3a2043020d52eac1f213624f40f2caa7d71133
Aaron Chafetz <achafetz@usaid.gov>
2021-07-22 16:36:21 -0400

    add PEPFAR countryname/iso code crosswalk for merging to stringency data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' PEPFAR COUNTRY ISO CODES
+#'
+#' A dataframe containing the PEPFAR country name and its associated ISO Code 3
+#'
+#' @format A data frame with 71 rows and 2 variables:
+#' \describe{
+#'   \item{countryname}{PEPFAR country name}
+#'   \item{iso}{3 letter ISO code}
+#' }
+#' @source \url{https://final.datim.org/}
+"pepfar_iso_map"
