ea8e3a43cadb7516aee1d60906fa679160c34aee
Aaron Chafetz <achafetz@usaid.gov>
2021-09-01 11:39:26 -0400

    clean up head matter and allow for pepfar only; remove progress/guessing

--- R/get_vax_countries.R
+++ R/get_vax_countries.R
@@ -1,23 +1,49 @@
 #' Import Our World in Data Vaccine rates
-#' @title Pull list of countries for which vax data exist
-#' @description Returns an object of the countries being tracked by Our World in Data
-#' @param url what is the url for the github repo containing vax info by country
+#' @title Import Our World in Data Vaccine rates
+#' 
+#' Pull list of countries for which vax data exist
+#' @return Returns an object of the countries being tracked by Our World in Data
+#' @param pepfar_only pepfar_only limit to just PEPFAR countries, default = TRUE
 #' @export 
+#' @family vax
 #' @examples
 #' \dontrun{
-#' vax_list <- get_vax_countries(url)
+#' #all countries
+#'   cntry_list <- get_vax_countries()
+#'   df_vax <- get_vax_data(cntry_list)
+#' #pepfar only countries
+#'   cntry_list <- get_vax_countries()
 #' }
 
 
 # Pull down the ous for which vaccine data exist
 
-get_vax_countries <- function(url = owid_vax_url) {
+get_vax_countries <- function(pepfar_only = TRUE) {
   
   # fetch all the countries in the repo, make a list to pass to map_dfr
   cntry_obj <- 
-    rvest::read_html(url) %>% 
+    rvest::read_html(owid_vax_url) %>% 
     rvest::html_nodes(xpath = '//*[@class = "js-navigation-open Link--primary"]') %>% 
-    xml2::xml_text()
+    xml2::xml_text() %>% 
+    stringr::str_remove("\\.csv$")
+  
+  if(pepfar_only == TRUE)
+    cntry_obj <-
+      c("Angola", "Myanmar", "Cambodia", "China", "India", "Indonesia", 
+        "Kazakhstan", "Kyrgyzstan", "Laos", "Nepal", "Papua New Guinea", 
+        "Philippines", "Tajikistan", "Thailand", "Turkmenistan", 
+        "Uzbekistan", "Botswana", "Brunei", "Cameroon", "Cote d'Ivoire", 
+        "Democratic Republic of Congo", "Dominican Republic", "Eswatini", 
+        "Ethiopia", "Haiti", "Kenya", "Lesotho", "Malawi", "Mozambique", 
+        "Namibia", "Nigeria", "Rwanda", "South Africa", "South Sudan", 
+        "Tanzania", "Uganda", "Ukraine", "Vietnam", "Benin", 
+        "Burkina Faso", "Ghana", "Liberia", "Mali", "Senegal", 
+        "Sierra Leone", "Togo", "Antigua and Barbuda", "Bahamas", 
+        "Barbados", "Belize", "Brazil", "Colombia", "Costa Rica", 
+        "Dominica", "El Salvador", "Grenada", "Guatemala", "Guyana", 
+        "Honduras", "Jamaica", "Nicaragua", "Panama", "Peru", 
+        "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", 
+        "Suriname", "Trinidad and Tobago", "Venezuela", "Zambia", "Zimbabwe")
   
   return(cntry_obj)  
 }
--- R/get_vax_data.R
+++ R/get_vax_data.R
@@ -1,27 +1,46 @@
 #' Import OWID vaccine data
 #' @title Fetch Our World In Data vaccine data
 #' @description Retrieves COVID-19 vaccination data for a given list of countries
-#' @param cntry list of countries that appear in the our world in data page (ends in .csv)
+#' @param cntry country, or list of countries, that appear in the our world in 
+#'   data page
+#' @family vax
+#' @return Returns data frame of total vaccinations  
 #' @export 
 #' @examples
 #' \dontrun{
-#' zmb <- get_vax_data(cntry = "Zambia.csv")
+#' #only one country
+#'   df_vax_zmb <- get_vax_data("Zambia")
+#' #only PEPFAR countries
+#'   cntry_list <- get_vax_countries()
+#'   df_vax_pepfar <- get_vax_data(cntry_list)
+#' #all countries
+#'   cntry_list <- get_vax_countries(pepfar_only = FALSE)
+#'   df_vax <- get_vax_data(cntry_list)
 #' }
 
 
 # Pull and compress list of ous into a dataframe
 get_vax_data <- function(cntry){
   
-  suppressMessages(
-    df <-  
-   cntry %>%
-   purrr::map_dfr(~make_url(.x) %>% readr::read_csv())
+  tryCatch(
+    {
+      df <- cntry %>%
+        add_csv_stub() %>% 
+        purrr::map_dfr(~make_url(.x) %>% 
+                         readr::read_csv(show_col_types = FALSE, 
+                                         progress = FALSE))
+      
+      df <- df %>% 
+        dplyr::rename(countryname = location) %>% 
+        dplyr::mutate(countryname = dplyr::recode(countryname,
+                                                  "Myanmar" = "Burma",
+                                                  "Brunei" = "Burundi",
+                                                  "Democratic Republic of Congo" = "Democratic Republic of the Congo"))
+      
+      return(df)
+    }
   )
-  
- df <- 
-   df %>% 
-   dplyr::rename(countryname = location)
-  
- return(df)
+    
 }
 
+try
